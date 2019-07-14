44f566099d7d6ac7681d7708246c7c94041767f7
cleberspirlandeli <contato.cleberrezende@gmail.com>
2019-07-14 03:06:28 -0300

    first video

---
+++ SpaUserControl/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using SpaUserControl.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User(
+                "Cleber Rezende", 
+                "test@test.com");
+
+            try
+            {
+                user.SetPassword("123456", "123456");
+
+                Console.WriteLine(user.Name);
+                Console.WriteLine(user.Password);
+
+                var password = user.ResetPassword();
+
+                Console.WriteLine(password);
+                Console.WriteLine(user.Password);
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+
+            }
+
+        }
+    }
+}
---
+++ SpaUserControl/SpaUserControl.Domain/Models/User.cs
@@ -0,0 +1,45 @@
+﻿using SpaUserControl.Common.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaUserControl.Domain.Models
+{
+    public class User
+    {
+        public User(string name, string email)
+        {
+            this.Name = name;
+            this.Email = email;
+        }
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+
+        public void SetPassword(string password, string confirmPassword)
+        {
+            AssertionConcern.AssertArgumentNotNull(password, "Senha inválida.");
+            AssertionConcern.AssertArgumentNotNull(confirmPassword, "As senhas não coincidem.");
+            AssertionConcern.AssertArgumentEquals(password, confirmPassword, "As senhas não coincidem.");
+            AssertionConcern.AssertArgumentLength(password, 6, 20, "A senha deve conter de 6 a 20 caracteres.");
+
+            this.Password = PasswordAssertionConcern.Encrypt(password);
+        }
+
+        public string ResetPassword()
+        {
+            string password = Guid
+                                .NewGuid()
+                                .ToString()
+                                .Substring(0, 8);
+            this.Password = PasswordAssertionConcern.Encrypt(password);
+
+            return password;
+        }
+
+
+    }
+}
